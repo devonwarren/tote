026884e69708b6db99a340f5998ea08ae4f16e54
Devon Warren <devon.warren@gmail.com>
2016-08-24 20:43:44 -0400

    initial commit

---
+++ tote/settings/dev.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '*$wr3clty=5u=s6qmruyjxfn$j=b*yri)gj11fa&omsbc(54*%'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
