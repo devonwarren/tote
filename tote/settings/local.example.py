8fb2b68594f958196f7f575d2fce23b6b0f64390
Devon Warren <devon.warren@gmail.com>
2016-10-16 16:35:43 -0400

    prepare production settings with postgresql

--- tote/settings/local.example.py
+++ tote/settings/local.example.py
@@ -4,3 +4,6 @@ TWITTER_KEYS = {
     'access_token_key': '',
     'access_token_secret': '',
 }
+
+# uncomment if using postgres and put in password
+# DATABASES['default']['password'] = ''
--- tote/settings/production.py
+++ tote/settings/production.py
@@ -4,6 +4,17 @@ from .base import *
 
 DEBUG = False
 
+# password is set in local 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tote',
+        'USER': 'tote',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 try:
     from .local import *
 except ImportError:
